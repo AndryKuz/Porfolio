a108f93a608d2574a8279dc39ede05d8ecc24995
AnryKuz <femon1992@gmail.com>
2023-07-20 11:58:54 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+    
+    iconMenu.addEventListener('click', function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+        document.body.classList.toggle('_lock');
+        
+    });
+    
+}
